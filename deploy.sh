8c52d80612c4d6521449607390318cdc2f300121
Flavio.munis <flavio.munis42@gmail.com>
2023-03-09 17:05:29 -0300

    fixed error in the custom file creation

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Configurating EMACS..."
 cp -Tabs "$DIR/config/" "$HOME/"
 echo "Emacs Config Files Were Succefully Deployed as SymLinks in $HOME"
-touch "$HOME/" .custom.el
+touch "$HOME/.custom.el"
 echo "Created Custom Configurations File"
 
 echo "Emacs Configs Succefully Deployed!"
