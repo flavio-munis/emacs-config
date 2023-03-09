32df2475d023c90a752ebb251da3a790cc5381cd
Flavio.munis <flavio.munis42@gmail.com>
2023-03-09 11:11:45 -0300

    ignoring #* autosave files

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cp -Tabs "$DIR/config" "$HOME/"
+cp -Tabs "$DIR/config/" "$HOME/"
 echo "Emacs Config Files Were Succefully Deployed as SymLinks in $HOME"
