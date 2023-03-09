cf5946f7d669c1def48fc239706d621cace0956e
Flavio.munis <flavio.munis42@gmail.com>
2023-03-09 15:41:19 -0300

    script now creates a file for emacs custom variables

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,10 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+echo "Configurating EMACS..."
 cp -Tabs "$DIR/config/" "$HOME/"
 echo "Emacs Config Files Were Succefully Deployed as SymLinks in $HOME"
+touch "$HOME/" .custom.el
+echo "Created Custom Configurations File"
+
+echo "Emacs Configs Succefully Deployed!"
