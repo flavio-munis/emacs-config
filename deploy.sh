4eaeca75580d2443f0cfec2d49e468ba818d9269
flavio-munis <flavio.munis42@gmail.com>
2024-01-03 22:51:43 -0300

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/bash
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DEPLOY_DIR="$HOME/.emacs.d"
 
 echo "Configurating EMACS..."
-cp -Tabs "$DIR/config/" "$HOME/"
-echo "Emacs Config Files Were Succefully Deployed as SymLinks in $HOME"
-touch "$HOME/.custom.el"
-echo "Created Custom Configurations File"
-
-echo "Emacs Configs Succefully Deployed!"
+cp -Tabs "$DIR/config/" $DEPLOY_DIR
+echo "Emacs Config Files Were Succefully Deployed as SymLinks in $DEPLOY_DIR"
