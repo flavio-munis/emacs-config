7750cf27bb19d4ceeedbe28143460af00366d1c7
flavio-munis <flavio.munis42@gmail.com>
2024-01-03 22:52:02 -0300

    basic functionalities added

--- config/init.el
+++ config/init.el
@@ -1,3 +1,17 @@
+;; Sets Package Manager as Melpa
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+;; Checks if use-package is installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Sets config.org as the configuration file for emacs
 (org-babel-load-file
  (expand-file-name
   "config.org"
