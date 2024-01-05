302cec315d00421989296b4450e8f287ad6c9706
flavio-munis <flavio.munis42@gmail.com>
2024-01-04 22:26:07 -0300

    org mode table of contents and bullet mode added

--- config/init.el
+++ config/init.el
@@ -27,7 +27,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ispell-dictionary nil)
- '(package-selected-packages '(which-key zenburn-theme use-package)))
+ '(package-selected-packages '(org-bullets toc-org which-key zenburn-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
