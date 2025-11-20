1f19b69b0015b9ed2d12e4a8f82c1a8053984805
flavio-munis <flavio.munis42@gmail.com>
2025-11-20 08:01:38 -0300

    C-x k now kills current buffer

--- config/init.el
+++ config/init.el
@@ -44,6 +44,6 @@
  ;; If there is more than one, they won't work right.
  )
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
-(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;;(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
 (put 'downcase-region 'disabled nil)
